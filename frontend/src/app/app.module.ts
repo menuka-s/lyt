728832b88d39e3c59d33bcf50b2e30fa7e616ff5
menuka-s <menuka.samaranayake@gmail.com>
2017-08-18 18:56:28 -0400

    Set up app

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+//Node Imports
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//App Component Imports
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+//Routes
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+];
+
+//NgModule
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
